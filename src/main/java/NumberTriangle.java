683435543a99e1b72a2e7d2e3f9515a9b5debdf1
yarieskl <yangloukaskeliu@gmail.com>
2025-10-03 21:03:24 -0400

    Update NumberTriangle.java

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -89,7 +89,16 @@ public class NumberTriangle {
      */
     public int retrieve(String path) {
         // TODO implement this method
-        return -1;
+        if (path == null) {
+            return this.root;
+        }
+        else if (path.charAt(0) == 'l' && this.left != null) {
+            this.left.retrieve(path.substring(1));
+        }
+        else if (path.charAt(0) == 'r' && this.right != null) {
+            this.right.retrieve(path.substring(1));
+        }
+        return this.root;
     }
 
     /** Read in the NumberTriangle structure from a file.
@@ -116,8 +125,10 @@ public class NumberTriangle {
         // so might want a variable for that.
         NumberTriangle top = null;
 
-        String line = br.readLine();
+        String line =  br.readLine();
+
         while (line != null) {
+
 
             // remove when done; this line is included so running starter code prints the contents of the file
             System.out.println(line);
